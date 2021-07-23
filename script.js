df3dd00867dcd2fdf2368b1e58b282e8056f9cb7
Nelly Sugu <jonly03@gmail.com>
2021-07-22 22:51:14 -0400

    toggles right nav on load

--- script.js
+++ script.js
@@ -1,5 +1,27 @@
-`
-    <li class="nav-item">
-        <a class="nav-link active" aria-current="page" href="https://auth0books.herokuapp.com/login">Login</a>
-    </li>
-`;
+const baseURL = "https://auth0books.herokuapp.com";
+
+fetch(`${baseURL}/user/me`)
+  .then((res) => res.json())
+  .then(({ user }) => {
+    toggleRightNav(user);
+  });
+
+function toggleRightNav(user) {
+  let navItem = "";
+
+  if (user !== undefined) {
+    navItem = `
+            <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="https://auth0books.herokuapp.com/logout">Logout</a>
+            </li>
+        `;
+  } else {
+    navItem = `
+            <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="https://auth0books.herokuapp.com/login">Login</a>
+            </li>
+        `;
+  }
+
+  document.getElementById("right-nav").innerHTML = navItem;
+}
